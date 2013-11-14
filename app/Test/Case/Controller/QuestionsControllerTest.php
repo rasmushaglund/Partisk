cf8bdbe9bdb27eb6af5fb940605d7ba8ed8e845c
rasmushaglund <rasmus.haglund@gmail.com>
2013-11-15 00:03:07 +0100

    Added a sample test which can be used to create new tests

--- app/Test/Case/Controller/QuestionsControllerTest.php
+++ app/Test/Case/Controller/QuestionsControllerTest.php
@@ -1,7 +1,11 @@
 <?php
-App::uses('AppController', 'Controller');
+App::uses('AppController', 'Controller', 'QuestionController', 'Model', 'Question');
 
 class QuestionsControllerTest extends ControllerTestCase {
+
+  public $fixtures = array('app.question');
+
+  public $testQuestionData = array('title' => 'test', 'tags' => '');
 
  	public function testIndex() {
       $result = $this->testAction('/questions/index', array('return' => 'contents'));
@@ -19,7 +23,7 @@ class QuestionsControllerTest extends ControllerTestCase {
 
   public function testViewWithNonExistingId() {
     try {
-      $result = $this->testAction('/questions/view/1as241fa2s431241251243', array('return' => 'contents'));
+      $result = $this->testAction('/questions/view/0', array('return' => 'contents'));
       $this->fail('Calling a question with a invalid id should rise an exception');
     } catch (NotFoundException $e) {
       debug($e);
@@ -27,8 +31,73 @@ class QuestionsControllerTest extends ControllerTestCase {
   }
 
   public function testAddNotLoggedIn() {
-      //$this->Controller->expects($this->once())->method('referer');
-      $this->testAction('/questions/add');
+    $this->Controller = $this->generate('Questions', array(
+        'models' => array('Question' => array('save', 'create'))
+    ));
+
+    $this->controller->Question->expects($this->never())->method('save');
+    $this->controller->Question->expects($this->never())->method('create');
+    
+    $result = $this->testAction('/questions/add', array('return' => 'contents', 'method' => 'post', 'data' => array(
+      'Question' => $this->testQuestionData
+      )));
+
+    debug($result);
   }
+
+  public function testAddAdminLoggedIn() {
+    $this->Controller = $this->generate('Questions', array(
+        'components' => array('Auth' => array('user')),
+        'models' => array('Question' => array('save', 'create'))
+        ));
+
+    $this->Controller->Auth->staticExpects($this->any())->method('user')->will($this->returnCallback(array($this, 'authUserCallback')));
+
+    $this->controller->Question->expects($this->once())->method('save');
+    $this->controller->Question->expects($this->once())->method('create');
+    
+    $result = $this->testAction('/questions/add', array('return' => 'contents', 'method' => 'post', 'data' => array(
+      'Question' => $this->testQuestionData
+      )));
+    debug($result);
+  }
+
+  public function testDeleteNotLoggedIn() {
+    $this->Controller = $this->generate('Questions', array(
+        'models' => array('Question' => array('delete'))
+    ));
+
+    $this->controller->Question->expects($this->never())->method('delete');
+    
+    $result = $this->testAction('/questions/delete/1', array('return' => 'contents'));
+    debug($result);
+  }
+
+  public function testDeleteAdminLoggedIn() {
+    $this->Controller = $this->generate('Questions', array(
+        'components' => array('Auth' => array('user')),
+        'models' => array('Question' => array('save'))
+        ));
+
+    $this->Controller->Auth->staticExpects($this->any())->method('user')->will($this->returnCallback(array($this, 'authUserCallback')));
+    $this->controller->Question->expects($this->once())->method('save');
+    
+    $result = $this->testAction('/questions/delete/1', array('return' => 'contents'));
+    debug($result);
+  }
+
+  public function authUserCallback($param){
+    $user = array(
+        'id' => 1,
+        'username' => 'test',
+        'Role' => array('name' => 'admin')
+    );
+
+        if(empty($param)){
+            return $user;
+        } else {
+            return $user[$param];
+        }
+    }
 }
 ?>	
